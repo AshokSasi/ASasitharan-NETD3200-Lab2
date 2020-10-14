0558f8984863c381e85ed58f7bca883999121983
ASHOK SASITHARAN <ASHOK2801@hotmail.com>
2020-10-14 19:59:53 -0400

    added equipment table

---
+++ dbo.equipment.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[equipment]
+(
+	[name] NVARCHAR(MAX) NOT NULL, 
+    [empID] INT NOT NULL, 
+    [desc] NVARCHAR(MAX) NOT NULL, 
+    [phone] NVARCHAR(MAX) NOT NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([empID]) 
+)
